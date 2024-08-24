e3fa1cd4a2169749b07073e6ff969f10b5878e4c
merlos <merlos@users.noreply.github.com>
2024-08-24 06:22:52 +0300

    add motion_notify.sh

---
+++ bin/motion_notify.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# Telegram bot token and chat ID
+BOT_TOKEN="XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX"
+CHAT_ID="XXXXXXX"
+
+# Arguments passed by Motion
+FILE_PATH="$1"        # Full path to the saved file
+EVENT_NUMBER="$2"     # Event number
+CAMERA_ID="$3"        # Camera ID
+
+echo "motion_notify: $1 - $2 - $3"
+
+# Message to send with the file
+MESSAGE="Motion event detected. 
+Camera ID: $CAMERA_ID
+Event Number: $EVENT_NUMBER"
+
+# Send the file to Telegram
+curl -sS -F chat_id="$CHAT_ID" \
+     -F caption="$MESSAGE" \
+     -F document=@"$FILE_PATH" \
+     "https://api.telegram.org/bot$BOT_TOKEN/sendDocument" > /dev/null
