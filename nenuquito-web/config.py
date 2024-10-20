8f6857fbd760e5dac40b2d16e3f613779f466f22
merlos <merlos@users.noreply.github.com>
2024-10-20 18:12:19 +0300

    update config vars

--- nenuquito-web/config.py
+++ nenuquito-web/config.py
@@ -3,12 +3,18 @@ config = {}
 # Secret key for CSRF protection
 config['SECRET_KEY'] = 'Set your custom secret key here'
 
+# Cloud address
+config['CLOUD_URL']='http://cloud.lan/'
 
-# Pihole link
+# DNS link
 config['DNS_ADMIN_URL']='http://dns.lan/admin/'
 
 # Cloud address
-config['CLOUD_URL']='http://cloud.lan/'
+config['MOTION_URL']='http://motion/'
+
+# Motion URL
+config['MONITORING_URL']='http://monitoring/'
+
 
 # Path to the bin folder
 config['BIN_PATH']='./tests/bin/'
